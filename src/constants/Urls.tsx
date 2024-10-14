cfffb3199ccf065faf3e14a0b54e62d2cf939475
Johnathan <john@knowledgecatalyst.io>
2024-10-14 14:57:33 +0800

    update main url

--- src/constants/Urls.tsx
+++ src/constants/Urls.tsx
@@ -1,7 +1,7 @@
 export enum URLS {
-  INFO = "https://knowledgecatalyst.io",
+  INFO = "https://tradestreamx.io",
   CREATOR = "https://creator.tradestreamx.io/",
   GITHUB = "https://github.com/TradeTrust/tradetrust-website",
   DOCS = "https://docs.tradetrust.io/",
-  MAIN = "https://knowledgecatalyst.io",
+  MAIN = "https://tradestreamx.io",
 }
