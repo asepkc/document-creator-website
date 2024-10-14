e9f0a9355ee45dc9d8d29fba98e34455156ee96e
Johnathan <john@knowledgecatalyst.io>
2024-10-14 14:53:44 +0800

    update icon and url link

--- src/constants/Urls.tsx
+++ src/constants/Urls.tsx
@@ -1,6 +1,6 @@
 export enum URLS {
   INFO = "https://knowledgecatalyst.io",
-  CREATOR = "https://tradetrust-creator.knowledecatalyst.io/",
+  CREATOR = "https://creator.tradestreamx.io/",
   GITHUB = "https://github.com/TradeTrust/tradetrust-website",
   DOCS = "https://docs.tradetrust.io/",
   MAIN = "https://knowledgecatalyst.io",
